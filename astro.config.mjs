25f7295e8c9b6c2ecc61be8dcb60ec6e46aa0456
Rob Shea <robtshea@pm.me>
2026-08-26 15:30:40 +1000

    config updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://decoheron.com',
+  base: '/',
+  build: {
+    assets: 'assets', // Changes output from `_astro` to `assets`
+  },
+});
+
+
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
